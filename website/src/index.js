92460cc40f3e5ecb3842438a06e9e19316803913
Evan Ugarte <evanuxd@gmail.com>
2021-07-01 13:24:52 -0700

    Add Website

---
+++ website/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LedSign from './LedSign';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LedSign />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
